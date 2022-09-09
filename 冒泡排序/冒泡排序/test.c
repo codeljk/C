465a72554416d98467fb889be2b2a62eda4376ed
李金锴 <2505319290@qq.com>
2022-09-09 17:19:56 +0800

    冒泡排序

---
+++ 冒泡排序/冒泡排序/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int sz)
+{
+	int i;
+	for (i = 0; i < sz - 1; i++) {
+		int j;
+		for (j = 0; j < sz - 1 - i; j++) {
+			while (arr[j] > arr[j + 1]) {
+				int t = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = t;
+			}
+		}
+	}
+}
+
+print(int arr[], int sz)
+{
+	int i;
+	for (i = 0; i < sz; i++) {
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	int arr[] = { 1,3,5,7,9,2,4,6,8,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz);
+	print(arr, sz);
+	return 0;
+}
