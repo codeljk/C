a1182d5720e10f7cbdb3512ad97823b7dc7aef47
李金锴 <2505319290@qq.com>
2022-09-15 19:37:08 +0800

    .

---
+++ 2022-9-15/2022-9-15/源.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	//int i;
+	//int j = 1;
+	//for (i = 1; i < 10; i++) {
+	//	;
+	//}
+	//printf("%d", j);
+
+	//return 0;
+
+	//char arr[] = "bit";
+	//printf("%d\n",strlen(arr));
+
+	//return 0;
+
+	//char a = '\61';
+	//char b = '\x31';
+	//printf("%c %c\n", a, b);
+	//return 0;
+
+	//int n = 10;
+	//int a[n];
+	//return 0;
+
+	//char arr[] = "hello";
+	//printf("%d\n", strlen(arr));
+	//return 0;
+
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c' ,'\0'};
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	return 0;
+}
