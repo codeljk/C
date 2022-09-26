0b243f862dfee9226a2bfa5d6165b541e585ed77
李金锴 <2505319290@qq.com>
2022-09-26 21:30:09 +0800

    .

---
+++ 2022_9_26/2022_9_26/源.c
@@ -0,0 +1,33 @@
+#pragma warning (disable:6031)
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+	//char password[20] = { 0 };
+	//printf("请输入密码\n");
+	//scanf("%s", &password);
+	//printf("请确认密码\n");
+	//getchar();
+	//int ch = getchar();
+	//if (ch == 'Y') {
+	//	printf("确认成功\n");
+	//}
+	//else {
+	//	printf("确认失败\n");
+	//}
+
+	//int a = 'A';
+	//printf("%c\n", a);
+
+
+	//char a = -1;
+	//printf("%d\n", a);
+
+	while (getchar() != EOF) {
+		;
+	}
+
+	return 0;
+
+}
