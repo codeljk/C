0002633f481ca8cb0d83315170f3a1867c45727c
李金锴 <2505319290@qq.com>
2022-09-28 18:57:59 +0800

    .

--- 2022_9_28/2022_9_28/源.c
+++ 2022_9_28/2022_9_28/源.c
@@ -25,23 +25,34 @@ int main()
 	//return 0;
 
 
-	int i = 0;
-	for (i = 100; i <= 200; i++) {
-		int j = 0;
-		for (j = 2; j < (int)sqrt(i*1.0); j++) {
-			if (i % j == 0) {
-				break;
-			}
-		}
-		if ( i == j ) {
-			printf("%d\n",i);
-		}
+	//int i = 0;
+	//for (i = 100; i <= 200; i++) {
+	//	int j = 0;
+	//	for (j = 2; j < (int)sqrt(i*1.0); j++) {
+	//		if (i % j == 0) {
+	//			break;
+	//		}
+	//	}
+	//	if ( i == j ) {
+	//		printf("%d\n",i);
+	//	}
+	//}
+
+	//return 0;
+
+	char input[20] = { 0 };
+	system("shutdown -s -t 60");
+
+	again:
+	printf("请注意，你的电脑将在一分钟内关机，输入“我是猪”，停止关机\n");
+	scanf("%s", &input);
+	if (strcmp(input,"我是猪") == 0) {
+		system("shutdown -a");
 	}
-
+	else {
+		goto again;
+	}
 	return 0;
-
-
-
 
 
 
